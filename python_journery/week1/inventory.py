d7176975d384296b3b8e5230d828262463e6a1aa
Dambarudhar Sing <194763184+dsing-git@users.noreply.github.com>
2026-08-26 08:05:10 +0530

    inventory.py

---
+++ python_journery/week1/inventory.py
@@ -0,0 +1,11 @@
+def my_inventory():
+    inventory = {
+        "laptops": 2,
+        "mouse": 2,
+        "keyboards": 2,
+        "monitors": 1,
+        "ipad": 1,
+        "phones": 2,
+        "ps5": 1
+    }
+    return inventory
