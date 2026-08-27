65e61ffc6dde3a07b8eaad9ad0d38634ec8fd21f
Dambarudhar Sing <194763184+dsing-git@users.noreply.github.com>
2026-08-27 18:37:19 +0530

    Script to analyze .log & .txt file

---
+++ python_journery/week2/log_parser.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+log_dir = Path("C:\\ai_journey\\python_journery\\week2")
+
+for log_file in log_dir.glob("*.log"):
+    print(f"Found log file: {log_file.name}\n")
+
+    try: 
+        with open(log_file, "r") as f:
+            #log_data = f.readlines()
+            line_count = 0
+            error_count = 0
+            unique_words = set()
+            for line in f:
+                line_count += 1
+                if "ERROR" in line:
+                    error_count += 1
+                print(f"{line.strip()}")
+                unique_words.update(line.split())
+        print(f"\nTotal lines in log file: {line_count}")
+        print(f"\nERROR lines in log file: {error_count}")
+        print(f"\nAll unique words: {unique_words}")
+    except FileNotFoundError:
+        print("Log file not found.")
+        #log_data = []
+        f = []
+
